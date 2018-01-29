e53955141f5159775a987f936ca5be2e59e4099e
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-01-29 17:08:25 -0200

    Initializing the header construction.

--- src/header.php
+++ src/header.php
@@ -12,7 +12,7 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-white menu-shadow">
-    <a class="navbar-brand pt-0 mr-0" href="#">
+    <a class="navbar-brand pt-0" href="#">
         <img src="<?php echo get_template_directory_uri().'/assets/images/logo.svg' ?>" class="logo" style="width: 150px">
     </a>
     <div class="btn-group pt-1 ml-auto">
@@ -59,3 +59,22 @@
         </ul>
     </div>
 </nav>
+
+<div class="d-flex flex-column mt-2 d-lg-none">
+    <form class="form-inline tainacan-form-search">
+        <div class="input-group">
+            <input class="form-control ml-1 tainacan-input-search invisible" type="search">
+            <div class="input-group-prepend">
+                <!-- <div class="input-group-text border-0 p-0 text-heavy-metal bg-white mr-2">
+                    <i class="material-icons">search</i>
+                </div> -->
+                <button type="submit" class="btn btn-primary border-0 text-heavy-metal bg-white p-0 mx-2 pt-1" onclick=""><i class="material-icons">search</i></button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<div class="d-flex flex-column header">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/capa.png" class="img-fluid header-img">
+    <i class="material-icons float-right p-1 m-3 text-white bg-dark rounded-circle position-absolute" style="right: 0">settings</i>
+</div>
