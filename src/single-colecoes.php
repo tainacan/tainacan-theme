5ccf4d2cb9c5450d22a0eb5bd74841b7b8d2d480
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-02-15 15:35:52 -0200

    Create the Section Carousel highlight images.

--- src/single-colecoes.php
+++ src/single-colecoes.php
@@ -50,9 +50,9 @@
         <div class="col-12 d-flex border-bottom">
             <h4 class="text-jelly-bean">Imagens em destaque</h4> <span class="text-jelly-bean ml-auto align-self-center">Ver mais</span>
         </div>
-        <div class="row p-5">
+        <div class="row p-3 p-md-5">
             <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
-                <div class="carousel-inner row w-100 mx-auto p-5" role="listbox">
+                <div class="carousel-inner row w-100 mx-auto p-md-5" role="listbox">
                     <div class="carousel-item col-md-4 p-5 active">
                         <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400/000/fff?text=1" alt="slide 1">
                     </div>
@@ -78,12 +78,12 @@
                         <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=8" alt="slide 7">
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-                    <i class="material-icons">chevron_left</i>
+                <a class="carousel-control-prev text-scooter justify-content-start" href="#carouselExample" role="button" data-slide="prev">
+                    <i class="material-icons prev-play">play_arrow</i>
                     <span class="sr-only">Previous</span>
                 </a>
-                <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
-                    <i class="material-icons">chevron_right</i>
+                <a class="carousel-control-next text-scooter justify-content-end" href="#carouselExample" role="button" data-slide="next">
+                    <i class="material-icons">play_arrow</i>
                     <span class="sr-only">Next</span>
                 </a>
             </div>
