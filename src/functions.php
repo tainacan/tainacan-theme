c5c9cb6561d46977867b12c24c63d710dc9d5ef4
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-03-06 13:56:43 -0300

    move setup to funcions

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,32 @@
 <?php
+/**
+ * Setup Theme
+ */
 
-require get_template_directory() . '/functions/setup.php';
+if(!function_exists('tainacan_setup')) {
+
+    /**
+     * Execulta após o tema ser inicializado. 
+     * Isso é usado para a configuração básica do tema, registro dos recursos do tema e init hooks. 
+     * Observe que esta função está conectada ao gancho after_setup_theme, que é executado antes do gancho de init.
+     */
+    function tainacan_setup() {
+        /**
+        * Não exibe o menu do administrador na pagina do site. Mesmo quando estiver logado!
+        **/
+        show_admin_bar( false );
+
+        add_theme_support( 'html5', array( 'comment-list' ) );
+        
+        /**
+         * Desabilita o FTP na instalação de Plugins
+         */
+        define('FS_METHOD', 'direct');
+    }
+
+}
+add_action( 'after_setup_theme', 'tainacan_setup' );
+
 require get_template_directory() . '/functions/enqueues.php';
 require get_template_directory() . '/functions/pagination.php';
 require get_template_directory() . '/functions/single-functions.php';
