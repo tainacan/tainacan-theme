e003fcb66bc2c178ec2c96593d6339383e2d7968
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-03-14 09:22:29 -0300

    List post of blog page

--- src/template-parts/list-post.php
+++ src/template-parts/list-post.php
@@ -1,7 +1,19 @@
-<div class="card w-100">
-    <h5 class="card-header"><?php the_title(); ?></h5>
-    <div class="card-body">
-        <p class="card-text"><?php if(is_single()) : the_content(); else : the_excerpt(); endif; ?></p>
-        <a href="<?php the_permalink(); ?>" class="btn btn-primary">View more</a>
+<div class="media mb-4">
+    <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID()) ) : ?>
+        <a href="<?php the_permalink(); ?>">
+            <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'medium') ?>" alt="" class="img-fluid align-self-center mr-5">
+        </a>
+    <?php endif; ?>
+    <div class="media-body align-self-center">
+        <h5 class="mt-0 ">
+            <a href="<?php the_permalink(); ?>" class="font-weight-bold"><?php the_title(); ?></a> 
+            <time datetime="<?php the_time(); ?>" class="text-oslo-gray">
+                <?php the_time('j \\d\\e F \\d\\e Y'); ?>
+            </time> 
+            <?php if(has_category()) : ?>
+                <span class="text-oslo-gray px-2">/</span><span class="list-inline"><?php the_category(', '); ?></span>
+            <?php endif; ?>
+        </h5>
+        <?php if(is_single()) : the_content(); else : the_excerpt(); endif; ?>
     </div>
 </div>
--- src/template-parts/loop.php
+++ src/template-parts/loop.php
@@ -1,15 +1,13 @@
 <?php if(have_posts()): ?>
-    <div class="card-columns mt-3">
-        <div class="col-sm">
-            <?php while(have_posts()): 
-                the_post();
-                //Index
-                if(!is_single())
-                            get_template_part('template-parts/list-post');
-                else
-                    printf('<p>%2</p>', _e('Sorry, no posts matched your criteria.', 'tainacan-theme'));
-            endwhile; ?>
-        </div>
+    <div class="mt-3 px-sm-5 mx-sm-5 <?php if(!is_single()) : echo 'blog-posts'; endif; ?>">
+        <?php while(have_posts()): 
+            the_post();
+            //Index
+            if(!is_single())
+                        get_template_part('template-parts/list-post');
+            else
+                printf('<p>%2</p>', _e('Sorry, no posts matched your criteria.', 'tainacan-theme'));
+        endwhile; ?>
     </div>
     <nav class="mx-auto">
         <?php echo pagination_bst4(); ?>
