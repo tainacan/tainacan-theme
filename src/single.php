9aedb7d10766eb27a3a867ca222de62b9a698c3b
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-03-19 12:02:39 -0300

    add title page

--- src/single.php
+++ src/single.php
@@ -1,4 +1,12 @@
 <?php get_header(); ?>
+    <div class="px-sm-3">
+        <div class="border-bottom border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
+            <ul class="list-inline mb-0">
+                <li class="list-inline-item text-midnight-blue font-weight-bold">Title</li>
+                <li class="list-inline-item float-right"><a href=""><?php _e('Back'); ?></a></li>
+            </ul>
+        </div>
+    </div>
     <div class="row">
 
         <div class="col col-sm-9 mx-sm-auto p-3">
--- src/template-parts/loop.php
+++ src/template-parts/loop.php
@@ -1,4 +1,13 @@
 <?php if(have_posts()): ?>
+    <?php if(!is_single()) { $title = 'Blog'; } ?>
+    <div class="px-sm-3">
+        <div class="border-bottom border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
+            <ul class="list-inline mb-0">
+                <li class="list-inline-item text-midnight-blue font-weight-bold"><?php echo $title; ?></li>
+                <li class="list-inline-item float-right"><a href=""><?php _e('Back'); ?></a></li>
+            </ul>
+        </div>
+    </div>
     <div class="mt-3 px-sm-5 mx-sm-5 <?php if(!is_single()) : echo 'blog-posts'; endif; ?>">
         <?php while(have_posts()): 
             the_post();
