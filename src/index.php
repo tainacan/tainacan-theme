9b3b7463414eccd6b47bdb1c8442603c8cc7b00e
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-03-20 16:30:04 -0300

    initial sidebar

--- src/index.php
+++ src/index.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
-    <div class="col-sm">
-        <div id="content" role="main">
-            <?php get_template_part('template-parts/loop'); ?>
-        </div><!-- /#content -->
+    <div class="row">
+        <div class="col-sm">
+            <div id="content" role="main">
+                <?php get_template_part('template-parts/loop'); ?>
+            </div><!-- /#content -->
+        </div>
+        <?php get_sidebar(); ?>
     </div>
 <?php get_footer();
