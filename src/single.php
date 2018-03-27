892daeae530097510b6a408696cdc6508f6d704c
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-03-27 11:39:45 -0300

    Using only a loop for post list and post view

--- src/single.php
+++ src/single.php
@@ -1,26 +1,10 @@
 <?php get_header(); ?>
-    <div class="tainacan-title">
-        <div class="border-bottom border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
-            <ul class="list-inline mb-0">
-                <li class="list-inline-item text-midnight-blue font-weight-bold">Title</li>
-                <li class="list-inline-item float-right"><a href=""><?php _e('Back'); ?></a></li>
-            </ul>
-        </div>
-    </div>
     <div class="row">
-
-        <div class="col col-sm-9 mx-sm-auto p-3">
+        <div class="col col-sm-9 mx-sm-auto">
             <div id="content" role="main">
-                <?php if(have_posts()): 
-                    while(have_posts()): the_post(); ?>
-                        <?php get_template_part('template-parts/single-post', get_post_format()); 
-                    endwhile; 
-                else:
-                    wp_redirect(esc_url( home_url() ) . '/404', 404);
-                    exit;
-                endif; ?>
+                <?php get_template_part('template-parts/loop'); ?>
             </div><!-- /#content -->
         </div>
-
+        <?php get_sidebar(); ?>
     </div><!-- /.row -->
 <?php get_footer(); ?>
--- src/template-parts/loop.php
+++ src/template-parts/loop.php
@@ -1,21 +1,30 @@
 <?php if(have_posts()): ?>
-    <?php if(!is_single()) { $title = 'Blog'; } ?>
     <div class="tainacan-title">
         <div class="border-bottom border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
             <ul class="list-inline mb-0">
-                <li class="list-inline-item text-midnight-blue font-weight-bold"><?php echo $title; ?></li>
+                <li class="list-inline-item text-midnight-blue font-weight-bold">
+                    <?php 
+                        if(is_home()) echo 'Blog'; 
+                        elseif(is_single()) the_title(); 
+                    ?>
+                </li>
                 <li class="list-inline-item float-right"><a href=""><?php _e('Back'); ?></a></li>
             </ul>
         </div>
     </div>
-    <div class="mt-3 px-sm-5 mx-sm-5 <?php if(!is_single()) : echo 'blog-posts'; endif; ?>">
+    <div class="mt-3 <?php if(is_home()) echo 'px-sm-5 mx-sm-5 tainacan-list-post'; elseif(is_single()) echo 'tainacan-single-post'; ?>">
         <?php while(have_posts()): 
             the_post();
-            //Index
-            if(!is_single())
-                        get_template_part('template-parts/list-post');
-            else
-                printf('<p>%2</p>', _e('Sorry, no posts matched your criteria.', 'tainacan-theme'));
+            //List Post
+            if(is_home()){
+                get_template_part('template-parts/list-post');
+            }
+            //View Post
+            elseif(is_single()){
+                get_template_part('template-parts/single-post');
+            } else {
+                printf('<p>%2</p>', __('Sorry, no posts matched your criteria.', 'tainacan-theme'));
+            }
         endwhile; ?>
     </div>
     <?php echo tainacan_pagination(3); ?>
