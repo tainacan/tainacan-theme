05d64c9a5dc8a1e8621050c5dccff46191cb77a0
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-03 13:50:09 -0300

    Create archive to loop category, tags and archives

---
+++ src/archive.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+    <div class="row">
+        <div class="col-sm">
+            <div id="content" role="main">
+                <?php get_template_part('template-parts/loop'); ?>
+            </div><!-- /#content -->
+        </div>
+        <?php get_sidebar(); ?>
+    </div><!-- /.row -->
+<?php get_footer(); ?>
