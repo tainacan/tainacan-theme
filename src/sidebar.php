d6e938c682a291036e0754125368e6a2ccc80057
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-03 10:31:00 -0300

    fix responsive sidebar in tablet

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,4 +1,4 @@
-<div class="col col-sm-3 px-sm-5">
+<div class="col col-sm-4 px-sm-3 col-lg-3 px-lg-5">
     <div id="sidebar" class="py-4 mx-2 mx-sm-0 sidebar hidden-print">
         <?php dynamic_sidebar(); ?>
     </div>
