b2ff61046e1fd104f40ebbeee70a97ad899ee440
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-03 13:58:34 -0300

    author template in archive

--- src/author.php
+++ src/author.php
@@ -1,39 +1,10 @@
-<?php
-$userInfo = get_userdata( get_query_var('author'));
-$isAuthor = true;
-if (
-    !in_array('contributor', $userInfo -> roles) &&
-    !in_array('administrator', $userInfo -> roles) &&
-    !in_array('author', $userInfo -> roles) &&
-    !in_array('editor', $userInfo -> roles)
-) {
-    $isAuthor = false;
-    wp_redirect(esc_url( home_url() ) . '/404', 404);
-}
-?>
 <?php get_header(); ?>
     <div class="row">
-
         <div class="col-sm">
             <div id="content" role="main">
-                <header class="mb-4 border-bottom">
-                    <?php if ($isAuthor === true): ?>
-                        <h1>
-                            <?php _e('Posts by: ', 'tainacan-theme'); echo get_the_author_meta( 'display_name' ); ?>
-                        </h1>
-                    <?php endif; ?>
-                </header>
-                <?php if(have_posts()): ?>
-                    <?php get_template_part('template-parts/index-loop'); ?>
-                <?php else: ?>
-                    <?php get_template_part('template-parts/index-none'); ?>
-                <?php endif; ?>
-
+                <?php get_template_part('template-parts/loop'); ?>
             </div><!-- /#content -->
         </div>
-
         <?php get_sidebar(); ?>
-
-    </div> <!-- /.row -->
-
+    </div><!-- /.row -->
 <?php get_footer(); ?>
