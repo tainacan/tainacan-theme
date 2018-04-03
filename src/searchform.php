96d1ff3d5d604c3b2e658f5c4bac6da3ad2370b4
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-03 13:31:31 -0300

    fix search widget

--- src/searchform.php
+++ src/searchform.php
@@ -1,8 +1,8 @@
-<form action="">
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <div class="input-group">
-        <input class="form-control py-2 border-right-0 border" type="search" value="search" id="example-search-input">
+        <input class="form-control py-2 border-right-0 border" type="search" name="s" value="search" id="example-search-input">
         <span class="input-group-append">
-            <button class="btn btn-outline-secondary border-left-0 border bg-white" type="button">
+            <button class="btn btn-outline-secondary border-left-0 border bg-white" type="submit">
                 <i class="material-icons" style="line-height: inherit;">search</i>
             </button>
         </span>
