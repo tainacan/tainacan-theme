d66211b0909d6753c2af57313ab35b2de2408355
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-03 13:50:56 -0300

    Modify search template

--- src/search.php
+++ src/search.php
@@ -2,11 +2,9 @@
     <div class="row">
         <div class="col-sm">
             <div id="content" role="main">
-                <header class="mb-4 border-bottom">
-                    <h1><?php _e('Search Results for', 'tainacan-theme'); ?> &ldquo;<?php the_search_query(); ?>&rdquo;</h1>
-                </header>
                 <?php get_template_part('template-parts/loop'); ?>
             </div><!-- /#content -->
         </div>
+        <?php get_sidebar(); ?>
     </div><!-- /.row -->
 <?php get_footer(); ?>
