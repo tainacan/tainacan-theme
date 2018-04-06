28c67e85ad7856d8b7d2ffd2ebfc1a55e0bfcc5a
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-06 11:02:08 -0300

    style the sidebar footer

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,5 +1,5 @@
 <div class="col col-sm-4 px-sm-3 col-lg-3 px-lg-5">
     <div id="sidebar" class="py-4 mx-2 mx-sm-0 sidebar hidden-print">
-        <?php dynamic_sidebar(); ?>
+        <?php dynamic_sidebar('sidebar-right'); ?>
     </div>
 </div>
