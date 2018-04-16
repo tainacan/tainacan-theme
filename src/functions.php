2d802af435b56b5b36cbce1281d964c5c93e91cc
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-16 15:47:36 -0300

    Removed duplicate border on collapse

--- src/functions.php
+++ src/functions.php
@@ -79,7 +79,7 @@ function tainacan_widgets_footer_init() {
     register_sidebar( array(
         'name'          => __( 'Tainacan Sidebar Footer', 'tainacan-theme' ),
         'id'            => 'footer-1',
-        'before_widget' => '<li class="border border-white border-left-0 border-right-0 tainacan-side"><input type="checkbox" checked><i></i>',
+        'before_widget' => '<li class="border-left-0 border-right-0 tainacan-side"><input type="checkbox" checked><i></i>',
         'after_widget'  => '</li>',
         'before_title'  => '<h6 class="text-white font-weight-bold mb-lg-4">',
         'after_title'   => ' <i class="material-icons mt-2 symbol"></i></h6>',
