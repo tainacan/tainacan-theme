70db49b993b90b8445fba090ad92214ebcaed7c9
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-17 14:51:28 -0300

    nowrap to view items in pagination

--- src/functions/pagination.php
+++ src/functions/pagination.php
@@ -38,7 +38,7 @@ if ( ! function_exists( 'pagination_bst4' ) ) {
 		}
 		
 		echo '<div class="row margin-pagination justify-content-between border-top">';
-			printf('<div class="col-sm-3 d-none d-lg-block pl-0">Viewing Items: %d to %d from %d</div>', $count_max + 1, $count_max + $wp_query->post_count, $wp_query->found_posts);
+			printf('<div class="col-sm-3 d-none d-lg-block pl-0 view-items">Viewing Items: %d to %d from %d</div>', $count_max + 1, $count_max + $wp_query->post_count, $wp_query->found_posts);
 			echo '<div class="col-sm-5 pr-md-0">';
 				echo '<ul class="pagination justify-content-center justify-content-md-end">' . "\n";
 	 
