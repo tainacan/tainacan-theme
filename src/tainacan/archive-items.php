c24d32e015fad34799ae9bb0b506b6f0563c7f4b
Leo Germani <leogermani@hacklab.com.br>
2018-04-17 22:55:24 -0300

    archive-items template

---
+++ src/tainacan/archive-items.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+	<div class="row">
+        <div class="col col-sm-9 mx-sm-auto">
+            <div id="content" role="main">
+                <div id="taincan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
+            </div><!-- /#content -->
+        </div>
+    </div><!-- /.row -->
+<?php get_footer(); ?>
