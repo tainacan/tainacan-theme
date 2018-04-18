09a80276793a54fdc93c74c6d42b8bedc4044f01
Leo Germani <leogermani@hacklab.com.br>
2018-04-18 08:06:06 -0300

    fix div id

--- src/tainacan/archive-items.php
+++ src/tainacan/archive-items.php
@@ -2,7 +2,7 @@
 	<div class="row">
         <div class="col col-sm-9 mx-sm-auto">
             <div id="content" role="main">
-                <div id="taincan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
+                <div id="tainacan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
             </div><!-- /#content -->
         </div>
     </div><!-- /.row -->
