2295d6a99bc42940ed9704755c1d58221d8e6dfa
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-04-19 16:50:43 -0300

    change menu

--- src/header-menu.php
+++ src/header-menu.php
@@ -15,12 +15,12 @@
 </head>
 <body <?php body_class(); ?>>
         
-    <nav class="navbar navbar-expand-md navbar-light bg-white menu-shadow px-1">
-        <div class="d-md-flex ml-md-auto mr-md-4">
+<nav class="navbar navbar-expand-md navbar-light bg-white menu-shadow px-1">
+        <div class="container-fluid pr-lg-5">
             <?php echo tainacan_get_logo(); ?>
-            <div class="btn-group pt-1 ml-auto">
-                <button type="button" class="btn btn-link text-heavy-metal pr-1"><i class="material-icons">folder_open</i></button>
-                <button type="button" class="btn btn-link text-jelly-bean px-1 dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div class="btn-group ml-auto">
+                <button type="button" class="btn btn-link text-heavy-metal px-0"><i class="material-icons">person_outline</i><span><?php _e('User'); ?></span></button>
+                <button type="button" class="btn btn-link text-jelly-bean dropdown-toggle dropdown-toggle-split px-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span class="sr-only">Toggle Dropdown</span>
                 </button>
                 <div class="dropdown-menu tainacan-dropdown-menu">
@@ -30,32 +30,8 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Separated link</a>
                 </div>
-            </div>
-            <div class="btn-group pt-1">
-                <button type="button" class="btn btn-link text-heavy-metal pr-0 pl-1"><i class="material-icons">person_outline</i></button>
-                <button type="button" class="btn btn-link text-jelly-bean px-1 dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="sr-only">Toggle Dropdown</span>
-                </button>
-                <div class="dropdown-menu tainacan-dropdown-menu">
-                    <a class="dropdown-item" href="#">Trocar Senha</a>
-                    <a class="dropdown-item" href="#">Sair</a>
-                </div>
-            </div>
-            <button class="navbar-toggler text-heavy-metal border-0 px-0 pt-2 ml-auto" type="button" data-toggle="collapse" data-target="#menuTp" aria-controls="menuTp" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="material-icons">more_vert</i>
-            </button>
-            <div class="collapse navbar-collapse mt-sm-1 mr-sm-3" id="menuTp">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-heavy-metal" href="#"><i class="material-icons">notifications_none</i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-heavy-metal" href="#"><i class="material-icons">event</i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-heavy-metal" href="#"><i class="material-icons">help_outline</i></a>
-                    </li>
-                </ul>
+                <button type="button" class="btn btn-link text-heavy-metal px-1"><i class="material-icons">notifications_none</i></button>
+                <button type="button" class="btn btn-link text-heavy-metal pl-1 pr-lg-0"><i class="material-icons">help_outline</i></button>
             </div>
         </div>
     </nav>
