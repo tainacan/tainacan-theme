8b6c05c0464b2a8ddafb6696787b77b2a7f9749a
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-05-09 16:25:38 -0300

    change the layout of collections

--- src/tainacan/archive-items.php
+++ src/tainacan/archive-items.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div class="row">
-        <div class="col col-sm-9 mx-sm-auto">
+        <div class="col mx-sm-auto p-0">
             <div id="content" role="main">
                 <div id="tainacan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
             </div><!-- /#content -->
