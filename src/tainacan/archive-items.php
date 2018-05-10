04af344eba9759511d79b6bd0357737699f04d2c
mateuswetah <mateus.m.luna@gmail.com>
2018-05-10 14:09:58 -0300

    Adds class to archive to test site auto-loading.

--- src/tainacan/archive-items.php
+++ src/tainacan/archive-items.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div class="row">
-        <div class="col mx-sm-auto p-0">
+        <div class="col mx-sm-auto p-0 tainacan-list">
             <div id="content" role="main">
                 <div id="tainacan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
             </div><!-- /#content -->
