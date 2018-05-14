fd1256a4b318934fc73f6fe7843820bc657e0a13
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-05-14 12:25:19 -0300

    Header collection

--- src/tainacan/header-collection.php
+++ src/tainacan/header-collection.php
@@ -7,31 +7,32 @@
  */
 ?>
 
-<div class="t-collection--background-title p-5"></div>
-<div class="container-fluid">
-    <div class="row t-collection--info ml-sm-5">
-        <div class="col pr-0">
-            <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid d-none d-md-block">
-        </div>
-        <div class="col-10 pl-0">
-            <h2 class="mt-3 mt-md-0 t-collection--info-title"><?php tainacan_the_collection_name(); ?></h2>
-            <div class="d-flex justify-content-between container ml-0 pl-0">
-                <div><?php tainacan_the_collection_description(); ?></div>
-                <div>
-                    <a href="#" class="">Icon 1</a> <a href="#">Icon 2</a>
+    <div class="t-collection--background-title"></div>
+    <div class="container-fluid">
+        <div class="row t-collection--info ml-sm-5">
+            <div class="col pr-0">
+                <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid d-none d-md-block border border-white">
+            </div>
+            <div class="col-10 pl-0">
+                <h2 class="mt-3 mt-md-0 t-collection--info-title text-white"><?php tainacan_the_collection_name(); ?></h2>
+                <div class="d-flex justify-content-between container ml-0 pl-0 position-absolute" style="top: 15px">
+                    <div><?php tainacan_the_collection_description(); ?></div>
+                    <div>
+                        <a href="#" class="">Icon 1</a> <a href="#">Icon 2</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
     <?php
-    $bread = "<ol class='breadcrumb pb-0 mb-1' style='background: transparent'>";
-    $bread .= "<li class='breadcrumb-item'><a href='#'>Home</a></li>";
-    $bread .= "<li class='breadcrumb-item'><a href='#'>Site</a></li>";
-    $bread .= "<li class='breadcrumb-item active' aria-current='page'>Blog</li>";
-    $bread .= "</ol>";
-    if(has_nav_menu('navMenubelowHeader')) : ?>
+        $bread = "<ol class='breadcrumb pb-0 mb-1' style='background: transparent'>";
+        $bread .= "<li class='breadcrumb-item'><a href='#'>Home</a></li>";
+        $bread .= "<li class='breadcrumb-item'><a href='#'>Site</a></li>";
+        $bread .= "<li class='breadcrumb-item active' aria-current='page'>Blog</li>";
+        $bread .= "</ol>";
+    ?>
+    <?php if(has_nav_menu('navMenubelowHeader')) : ?>
         <nav class="navbar navbar-expand-md navbar-light bg-white px-1 border-bottom" role="navigation">
             <div class="container-fluid max-large">
                 <?php /* if(wp_is_mobile()) echo $bread; */ ?>
@@ -55,7 +56,3 @@
             </div>
         </nav>
     <?php endif; ?>
-    <!-- <nav aria-label="breadcrumb" class="d-none d-md-flex">
-        <?php //echo $bread; ?>
-    </nav> -->
-    <div class="container-fluid mt-5 max-large">
