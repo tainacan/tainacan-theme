92446cd277a85c80940b93130c32a32a392a7061
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-05-21 13:27:37 -0300

    initialize the single items of collections

---
+++ src/tainacan/single-items.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+<?php if(have_posts()): ?>
+	<?php while(have_posts()): the_post(); ?>
+	    <div class="tainacan-title mt-5">
+	        <div class="border-bottom border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
+	            <ul class="list-inline mb-1">
+	                <li class="list-inline-item text-midnight-blue font-weight-bold title-page">
+	                    <?php the_title(); ?>
+	                </li>
+	                <li class="list-inline-item float-right title-back"><a href="javascript:history.go(-1)"><?php _e('Back'); ?></a></li>
+	            </ul>
+	        </div>
+	    </div>
+	    <div class="mt-3 tainacan-single-post">
+            <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
+                <header class="mb-4">
+                    <div class="header-meta text-muted mb-5">
+                        <?php
+                            _e('By', 'tainacan-theme');
+                            echo ' ';
+                            the_author_posts_link();
+                            echo ' ';
+                            _e('on', 'tainacan-theme');
+                            echo ' ';
+                            tainacan_post_date();
+                        ?>
+                    </div>
+                    <?php //the_post_thumbnail(); ?>
+                </header>
+                <main class="tainacan-content text-tundora">
+                    <?php
+                        tainacan_the_document();
+                    ?>
+                </main>
+            </article>
+            <div class="row">
+                <!-- Container -->
+                <div class="col mt-3 mx-auto">
+                    <?php
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif; ?>
+                </div>
+            </div>
+	    </div>
+	<?php endwhile; ?>
+<?php else: ?>
+	<?php _e('Nothing found', 'tainacan-theme'); ?>
+<?php endif; ?>
+<?php get_footer(); ?>
