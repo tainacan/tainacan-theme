0a4ae2f2284b121e7136883ebf481edc2ebda61b
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-05-28 15:15:42 -0300

    deleted divs not used.

--- src/tainacan/archive-items.php
+++ src/tainacan/archive-items.php
@@ -1,10 +1,6 @@
 <?php get_header(); ?>
 <?php get_template_part('tainacan/header-collection'); ?>
-	<div class="row">
-        <div class="col mx-sm-auto p-0">
-            <div id="content" role="main">
-                <div id="tainacan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
-            </div><!-- /#content -->
-        </div>
-    </div><!-- /.row -->
+
+    <div id="tainacan-items-page" collection-id="<?php echo tainacan_get_collection_id(); ?>"></div>
+    
 <?php get_footer(); ?>
