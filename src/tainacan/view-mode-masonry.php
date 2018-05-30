cfcc78c9e7b5af2eacd63c170d9922ae07252991
Fabiano <fabianocarvalho15@gmail.com>
2018-05-30 17:24:45 -0300

    Change the thumbnail and added else to post.

--- src/tainacan/view-mode-masonry.php
+++ src/tainacan/view-mode-masonry.php
@@ -2,11 +2,15 @@
     <div class="tainacan-masonry-container card-columns p-3">
         <?php while (have_posts()): the_post(); ?>
                 <div class="card">
-                    <?php the_post_thumbnail('large'); ?>
+                    <?php the_post_thumbnail('medium'); ?>
                     <div class="card-body">
                         <h5 class="card-title"><?php the_title(); ?></h5>
                     </div>
                 </div>      
         <?php endwhile; ?>
+    </div>
+<?php else: ?>
+    <div class="tainacan-masonry-container">
+        Nenhum item encontrado
     </div>
 <?php endif; ?>
