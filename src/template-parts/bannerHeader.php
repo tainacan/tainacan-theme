db75bd158025a1de470c042e02de53420bce1a1a
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-06-14 10:52:05 -0300

    Change the opacity title

--- src/template-parts/bannerHeader.php
+++ src/template-parts/bannerHeader.php
@@ -14,7 +14,7 @@
 
 <div <?php if ( get_header_image() ) : ?>class="page-header header-filter clear-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter clear-filter" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
     <div class="container-fluid p-0 mb-5">
-        <div class="bg-white title-header">
+        <div class="bg-white-title title-header">
             <h1 class="mb-0">
                 <?php if(is_home())?> Blog <?php bloginfo('title');?>
             </h1>
