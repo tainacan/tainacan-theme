a9e389dbc7e7ae22cc1ac25fb5d433d9dcafc6c9
Fabiano <fabianocarvalho15@gmail.com>
2018-06-18 15:52:18 -0300

    align sidebar to title

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,5 +1,5 @@
 <div class="col-12 col-md-3">
-    <div id="sidebar" class="py-4 mx-2 mx-sm-0 sidebar hidden-print">
+    <div id="sidebar" class="py-4 pt-md-0 mx-2 mx-sm-0 sidebar hidden-print">
         <?php dynamic_sidebar('sidebar-right'); ?>
     </div>
 </div>
