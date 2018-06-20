7d449366014c58fd4217e736f497cc51f06afc09
Fabiano <fabianocarvalho15@gmail.com>
2018-06-20 14:12:35 -0300

    Header of Home Collection

--- src/tainacan/header-collection.php
+++ src/tainacan/header-collection.php
@@ -1,5 +1,5 @@
 <!-- Get the banner to display -->
-<?php get_template_part( 'template-parts/bannerHeader' ); ?>
+<?php get_template_part( 'template-parts/headerCollection' ); ?>
 
 <?php 
 /**
@@ -9,24 +9,6 @@
  * 
  */
 ?>
-
-    <!-- <div class="t-collection--background-title"></div> -->
-    <div class="container-fluid">
-        <div class="row t-collection--info ml-sm-5">
-            <div class="col pr-0">
-                <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid d-none d-md-block border border-white">
-            </div>
-            <div class="col-10 pl-0">
-                <h2 class="mt-3 mt-md-0 t-collection--info-title text-white"><?php tainacan_the_collection_name(); ?></h2>
-                <div class="d-flex justify-content-between container ml-0 pl-0 position-absolute" style="top: 15px">
-                    <div><?php tainacan_the_collection_description(); ?></div>
-                    <div>
-                        <a href="#" class="">Icon 1</a> <a href="#">Icon 2</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menuBellowBanner' ); ?>
---
+++ src/template-parts/headerCollection.php
@@ -0,0 +1,20 @@
+<?php 
+    /* if(wp_is_mobile())
+        echo tainacan_get_form_search(); */
+?>
+
+<div <?php if ( get_header_image() ) : ?>class="page-header header-filter clear-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter clear-filter" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
+    <div class="container-fluid px-0 bg-jelly-bean t-bg-collection">
+        <div class="row t-collection--info margin-one-column">
+            <div class="col-4 col-md-3 px-0 t-collection--col-3">
+                <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid border border-white position-absolute">
+            </div>
+            <div class="col-8 col-md-9 pl-0 t-collection--col-9">
+                <h2 class="mt-3 mt-md-0 t-collection--info-title text-white position-absolute"><?php tainacan_the_collection_name(); ?></h2>
+                <div class="container t-collection--info-description ml-0 pl-0 mt-3 text-white">
+                    <div style="word-wrap: break-word;"><?php tainacan_the_collection_description(); ?></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
