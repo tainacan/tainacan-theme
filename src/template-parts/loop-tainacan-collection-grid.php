508de4afb548fb72b986cb5a83bad15bf663e672
Fabiano <fabianocarvalho15@gmail.com>
2018-07-09 16:40:53 -0300

    Grid list on list collection

--- src/template-parts/loop-tainacan-collection-grid.php
+++ src/template-parts/loop-tainacan-collection-grid.php
@@ -1,17 +1,17 @@
 <?php if(have_posts()): ?>
-    <div class="tainacan-list-post">
-        <div class="row">
+    <div class="tainacan-list-post text-center">
+        <div class="row mx-auto">
             <?php while(have_posts()): the_post(); ?>
-                <div class="col col-md-3 mt-5">
-                    <a class="tainacan-list-collection--grid" href="<?php the_permalink(); ?>">
-                        <p class="tainacan-list-collection--grid-title text-black">     
+                <a class="tainacan-list-collection--grid-link mt-5" href="<?php the_permalink(); ?>">
+                    <div class="col tainacan-list-collection--grid">
+                        <p class="tainacan-list-collection--grid-title text-black text-left">
                             <?php the_title(); ?>           
                         </p>
                         <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID()) ) : ?>
                             <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'collection-list-grid') ?>" class="img-fluid tainacan-list-collection--grid-img" alt="">  
                         <?php endif; ?>
-                    </a>
-                </div> 
+                    </div> 
+                </a>
             <?php endwhile; ?>
         </div>
     </div>
