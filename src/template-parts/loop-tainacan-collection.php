018343b34ddbf0a1def9d3204c2c633890276d6d
Fabiano <fabianocarvalho15@gmail.com>
2018-07-09 16:41:20 -0300

    Solution of permalink in table list on list collection

--- src/template-parts/loop-tainacan-collection.php
+++ src/template-parts/loop-tainacan-collection.php
@@ -1,7 +1,7 @@
 <?php if(have_posts()): ?>
     <div class="mt-5 tainacan-list-post">
         
-        <table class="table table-hover">
+        <table class="table table-hover table-responsive">
             <thead>
                 <tr>
                     <th scope="col"><?php _e('Miniature'); ?></th>
@@ -13,21 +13,17 @@
             </thead>
             <tbody>
                 <?php while(have_posts()): the_post(); ?>
-                    
-                    <tr class="tainacan-list-collection" data-href="<?php the_permalink(); ?>">
+                    <tr class="tainacan-list-collection" onclick="location.href='<?php the_permalink(); ?>'">
                         <td class="collection-miniature">
                             <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID()) ) : ?>
-                                <a href="<?php the_permalink(); ?>" class="">
-                                    <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'collection-list-table') ?>" class="img-fluid rounded-circle" alt="">
-                                </a>
+                                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'collection-list-table') ?>" class="img-fluid rounded-circle" alt="">
                             <?php endif; ?>
                         </td>
-                        <td class="collection-title"><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></td>
+                        <td class="collection-title"><?php the_title(); ?></td>
                         <td class="collection-description text-oslo-gray"><?php the_excerpt(); ?></td>
                         <td class="collection-date text-oslo-gray"><?php tainacan_post_date(); ?></td>
                         <td class="collection-create-by text-oslo-gray"><?php _e('Created by', 'tainacan-theme'); ?> <?php the_author_posts_link(); ?></td>
                     </tr>
-                
                 <?php endwhile; ?>
         
             </tbody>
