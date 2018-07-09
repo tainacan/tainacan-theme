99ccbd5e8db315a2c15690ecbab8c13e5a861151
Fabiano <fabianocarvalho15@gmail.com>
2018-07-09 16:41:29 -0300

    Card list on list collection

--- src/template-parts/loop-tainacan-collection-cards.php
+++ src/template-parts/loop-tainacan-collection-cards.php
@@ -1,34 +1,32 @@
 <?php if(have_posts()): ?>
-    <div class="mt-5 tainacan-list-post">
-        
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col"><?php _e('Miniature'); ?></th>
-                    <th scope="col"><?php _e('Title'); ?></th>
-                    <th scope="col"><?php _e('Author'); ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while(have_posts()): the_post(); ?>
-                    
-                    <tr class="tainacan-list-collection" data-href="<?php the_permalink(); ?>">
-                        <td class="collection-miniature">
+    <div class="tainacan-list-post">
+        <div class="row justify-content-center">
+            <?php while(have_posts()): the_post(); ?>
+            <a class="tainacan-list-collection--card-link mt-5" href="<?php the_permalink(); ?>">
+                <div class="card tainacan-list-collection--card border-0 mx-1">
+                    <div class="card-header text-black border-0">
+                        <?php the_title(); ?>
+                    </div>
+                    <div class="row ">
+                        <div class="col col-md-6">
                             <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID()) ) : ?>
-                                <a href="<?php the_permalink(); ?>" class="">
-                                    <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'collection-list-table') ?>" class="img-fluid rounded-circle" alt="">
-                                </a>
+                                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'collection-list-card') ?>" class="card-img-top-left tainacan-list-collection--card-img rounded-0" alt="">  
                             <?php endif; ?>
-                        </td>
-                        <td class="collection-title"><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></td>
-                        <td class="collection-create-by text-oslo-gray"><?php _e('Created by', 'tainacan-theme'); ?> <?php the_author_posts_link(); ?></td>
-                    </tr>
-                
-                <?php endwhile; ?>
-        
-            </tbody>
-        </table>
-        
+                        </div>
+                        <div class="col col-md-6 px-3">
+                            <div class="card-block p-0">
+                                <p class="card-text text-oslo-gray"><?php echo get_the_excerpt(); ?></p>
+                                <p class="card-text text-oslo-gray"> 
+                                    <?php _e('Create by: ');?> <?php the_author(); ?><br>
+                                    <?php _e('Date: ');?> <?php tainacan_post_date(); ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <?php endwhile; ?>
+        </div>
     </div>
 
     <?php echo tainacan_pagination(3); ?>
