0c3902c2aa86de1f39aa62d8b1b0950aa130af4c
Fabiano <fabianocarvalho15@gmail.com>
2018-07-10 15:38:41 -0300

    change margin of grid view in collection list

--- src/template-parts/loop-tainacan-collection-grid.php
+++ src/template-parts/loop-tainacan-collection-grid.php
@@ -2,7 +2,7 @@
     <div class="tainacan-list-post text-center container px-md-0">
         <div class="row mx-auto">
             <?php while(have_posts()): the_post(); ?>
-                <a class="tainacan-list-collection--grid-link mt-5 mr-2" href="<?php the_permalink(); ?>">
+                <a class="tainacan-list-collection--grid-link mt-5 mr-3" href="<?php the_permalink(); ?>">
                     <div class="col tainacan-list-collection--grid px-0">
                         <p class="tainacan-list-collection--grid-title text-black text-left">
                             <?php the_title(); ?>           
