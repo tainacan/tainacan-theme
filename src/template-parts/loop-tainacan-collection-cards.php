25ff28c0207d74847f092aec7b0d8fd1fd942689
Fabiano <fabianocarvalho15@gmail.com>
2018-07-10 12:38:03 -0300

    Align card list of collection list

--- src/template-parts/loop-tainacan-collection-cards.php
+++ src/template-parts/loop-tainacan-collection-cards.php
@@ -1,19 +1,19 @@
 <?php if(have_posts()): ?>
-    <div class="tainacan-list-post">
-        <div class="row justify-content-center">
+    <div class="tainacan-list-post container">
+        <div class="row mx-auto">
             <?php while(have_posts()): the_post(); ?>
-            <a class="tainacan-list-collection--card-link mt-5" href="<?php the_permalink(); ?>">
-                <div class="card tainacan-list-collection--card border-0 mx-1">
+            <a class="tainacan-list-collection--card-link mt-5 mx-1" href="<?php the_permalink(); ?>">
+                <div class="card tainacan-list-collection--card border-0">
                     <div class="card-header text-black border-0">
                         <?php the_title(); ?>
                     </div>
                     <div class="row ">
-                        <div class="col col-md-6">
+                        <div class="col col-md-6 pr-0">
                             <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID()) ) : ?>
                                 <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'collection-list-card') ?>" class="card-img-top-left tainacan-list-collection--card-img rounded-0" alt="">  
                             <?php endif; ?>
                         </div>
-                        <div class="col col-md-6 px-3">
+                        <div class="col col-md-6 px-0">
                             <div class="card-block p-0">
                                 <p class="card-text text-oslo-gray"><?php echo get_the_excerpt(); ?></p>
                                 <p class="card-text text-oslo-gray"> 
