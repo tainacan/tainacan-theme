052af7b218125e562d7b98bd14c5433a5d2f3a61
Fabiano <fabianocarvalho15@gmail.com>
2018-07-11 15:39:43 -0300

    Change the truncate title on banner to class of bootstrap

--- src/template-parts/bannerHeader.php
+++ src/template-parts/bannerHeader.php
@@ -6,9 +6,12 @@
 <div <?php if ( get_header_image() ) : ?>class="page-header header-filter clear-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter clear-filter align-items-center" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
     <div class="container-fluid p-0 ph-title-description">
         <div class="bg-white-title title-header <?php if(is_singular() || is_archive()) { echo 'singular-title'; }?>">
-            <h1 class="mb-0">
+            <h1 class="mb-0 text-truncate">
                 <?php if(is_home()) { ?> Blog <?php bloginfo('title'); }?>
-                <?php if(is_singular()) { echo mb_strimwidth(get_the_title(),0,25,'...');/* the_title(); */ }?>
+                <?php if(is_singular()) { 
+                    /* echo mb_strimwidth(get_the_title(),0,25,'...'); */
+                    the_title(); 
+                }?>
                 <?php if(is_archive()) { ?> <?php echo get_the_archive_title(); }?>
             </h1>
             <?php if(is_home()) {?> <span><?php bloginfo('description'); }?></span>
