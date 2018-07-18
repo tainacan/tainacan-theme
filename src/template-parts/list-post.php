44a0457009e4c92240b198bb2486b959fdd6abb5
Fabiano <fabianocarvalho15@gmail.com>
2018-07-18 16:55:09 -0300

    Fix list post and revised

--- src/template-parts/list-post.php
+++ src/template-parts/list-post.php
@@ -1,14 +1,14 @@
-<div class="row blog-post mb-4">
-    <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID()) ) : ?>
+<div class="row blog-post mb-3">
+    <?php if ( has_post_thumbnail() && get_the_post_thumbnail_url(get_the_ID(),'theme-list-post') ) : ?>
         <div class="col-xs-12 col-md-4 blog-thumbnail align-self-center text-center mb-4 mb-md-0">
-            <a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'medium') ?>" class="img-fluid" alt=""></a>
+            <a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'theme-list-post') ?>" class="img-fluid" alt=""></a>
         </div>
     <?php endif; ?>
-    <div class="col-xs-12 blog-content <?php if ( has_post_thumbnail() ) :?>col-md-8 blog-flex<?php else : ?>col-md-12<?php endif; ?>">
+    <div class="col-xs-12 blog-content <?php if ( has_post_thumbnail() ) :?>col-md-8 blog-flex<?php else : ?>col-md-12<?php endif; ?> align-self-center">
         <h3 class="mb-3">
             <a href="<?php the_permalink(); ?>" class="font-weight-bold"><?php the_title(); ?></a>
         </h3>
-        <?php echo '<p class="text-black">'.wp_trim_words( get_the_content(), 63, '...' ).'</p>'; ?>
+        <?php echo '<p class="text-black">'.wp_trim_words( get_the_excerpt(), 28, '...').'</p>'; ?>
         <time><?php the_time('j M Y \\a\\t g:i'); echo ', '; _e('for');?></time> <?php echo ' '; the_author_posts_link(); ?>
         <a href="<?php the_permalink(); ?>" class="readmore float-right"><?php _e('Read more...'); ?></a>
     </div>
