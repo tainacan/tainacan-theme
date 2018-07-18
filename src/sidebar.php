bc51debe056e0d4e21c32220ccf5e3d9f61e6df5
Fabiano <fabianocarvalho15@gmail.com>
2018-07-18 16:54:22 -0300

    sidebar display bellow page on ipad and tablet

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-md-3 border-top border-jelly-bean sidebar-border">
+<div class="col-12 col-lg-3 border-top border-jelly-bean sidebar-border">
     <div id="sidebar" class="py-4 pt-md-0 mx-2 mx-sm-0 sidebar hidden-print">
         <?php dynamic_sidebar('sidebar-right'); ?>
     </div>
