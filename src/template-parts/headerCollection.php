bd4a09388fba643893b84eba3a2c663e73d6a53b
Fabiano <fabianocarvalho15@gmail.com>
2018-07-19 12:37:18 -0300

    Fix header collection

--- src/template-parts/headerCollection.php
+++ src/template-parts/headerCollection.php
@@ -3,7 +3,7 @@
         echo tainacan_get_form_search(); */
 ?>
 
-<div <?php if ( get_header_image() ) : ?>class="page-header header-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter clear-filter page-collection" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
+<div <?php if ( get_header_image() ) : ?>class="page-header header-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter page-collection" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
     <div class="container-fluid px-0 bg-jelly-bean t-bg-collection">
         <div class="row t-collection--info margin-one-column">
             <div class="col-4 col-md-3 px-0 t-collection--col-3">
@@ -12,9 +12,10 @@
             <div class="col-8 col-md-9 pl-0 t-collection--col-9">
                 <h2 class="mt-3 mt-md-0 t-collection--info-title text-white position-absolute"><?php tainacan_the_collection_name(); ?></h2>
             </div>
-        </div>
-        <div class="row t-collection--info-description ml-0 pl-0 mt-3 text-white margin-one-column">
-            <p class="t-collection--info-description-text"><?php tainacan_the_collection_description(); ?></p>
+            <div class="col-4 col-md-3 px-0 t-collection--col-3"></div>
+            <div class="col-8 col-md-9 pl-0 t-collection--col-9">
+                <p class="text-white"><?php tainacan_the_collection_description(); ?></p>
+            </div>
         </div>
     </div>
 </div>
