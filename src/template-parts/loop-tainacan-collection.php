2a44403571f3b269d3f2bb994d0e56935fdbf72b
Fabiano <fabianocarvalho15@gmail.com>
2018-07-19 09:33:04 -0300

    Remove date and author of table list in collection list

--- src/template-parts/loop-tainacan-collection.php
+++ src/template-parts/loop-tainacan-collection.php
@@ -4,11 +4,11 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col"><?php _e('Miniature'); ?></th>
+                    <th scope="col"><?php //_e('Miniature'); ?></th>
                     <th scope="col"><?php _e('Title'); ?></th>
                     <th scope="col"><?php _e('Description'); ?></th>
-                    <th scope="col"><?php _e('Date'); ?></th>
-                    <th scope="col"><?php _e('Author'); ?></th>
+                    <!-- <th scope="col"><?php //_e('Date'); ?></th>
+                    <th scope="col"><?php //_e('Author'); ?></th> -->
                 </tr>
             </thead>
             <tbody>
@@ -29,10 +29,10 @@
                                 </div>
                             <?php endif; ?>
                         </td>
-                        <td class="collection-title text-black"><?php the_title(); ?></td>
-                        <td class="collection-description text-oslo-gray"><?php the_excerpt(); ?></td>
-                        <td class="collection-date text-oslo-gray"><?php echo get_the_date(); ?></td>
-                        <td class="collection-create-by text-oslo-gray"><?php _e('Created by', 'tainacan-theme'); ?> <?php the_author_posts_link(); ?></td>
+                        <td class="collection-title text-black text-truncate"><?php the_title(); ?></td>
+                        <td class="collection-description text-oslo-gray text-truncate"><?php the_excerpt(); ?></td>
+                        <!-- <td class="collection-date text-oslo-gray"><?php //echo get_the_date(); ?></td>
+                        <td class="collection-create-by text-oslo-gray"><?php //_e('Created by', 'tainacan-theme'); ?> <?php the_author_posts_link(); ?></td> -->
                     </tr>
                 <?php endwhile; ?>
         
