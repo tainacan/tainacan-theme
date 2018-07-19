f59c9c60179332abd4b3dd56f6369463dc3f8f49
Fabiano <fabianocarvalho15@gmail.com>
2018-07-19 15:48:35 -0300

    added for when there is an image

--- src/template-parts/headerCollection.php
+++ src/template-parts/headerCollection.php
@@ -7,7 +7,17 @@
     <div class="container-fluid px-0 bg-jelly-bean t-bg-collection">
         <div class="row t-collection--info margin-one-column">
             <div class="col-4 col-md-3 px-0 t-collection--col-3">
-                <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid border border-white position-absolute">
+                <?php if(has_post_thumbnail(tainacan_get_collection_id())) : ?>
+                    <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid border border-white position-absolute">
+                <?php else : ?>
+                    <div class="image-placeholder rounded-circle border border-white position-absolute">
+                        <h4 class="text-center">
+                        <?php 
+                            tainacan_the_collection_name();
+                        ?>
+                        </h4>
+                    </div>
+                <?php endif; ?>
             </div>
             <div class="col-8 col-md-9 pl-0 t-collection--col-9">
                 <h2 class="mt-3 mt-md-0 t-collection--info-title text-white position-absolute"><?php tainacan_the_collection_name(); ?></h2>
