4470bfdd9c6477ffa999e952cf6c0ec1c1237990
Fabiano <fabianocarvalho15@gmail.com>
2018-07-20 12:14:50 -0300

    Revised grid view on collection list

--- src/template-parts/loop-tainacan-collection-grid.php
+++ src/template-parts/loop-tainacan-collection-grid.php
@@ -1,5 +1,5 @@
 <?php if(have_posts()): ?>
-    <div class="tainacan-list-post text-center container px-md-0 mt-5">
+    <div class="tainacan-list-post px-md-0 mt-5">
         <div class="tainacan-list-collection--container-grid justify-content-center">
             <?php while(have_posts()): the_post(); ?>
                 <a class="tainacan-list-collection--grid-link" href="<?php the_permalink(); ?>">
@@ -11,7 +11,7 @@
                             <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'tainacan-medium') ?>" class="img-fluid tainacan-list-collection--grid-img" alt="">  
                         <?php else : ?>
                             <div class="image-placeholder">
-                                <h4>
+                                <h4 class="text-center">
                                     <?php echo tainacan_get_initials(get_the_title()); ?>
                                 </h4>
                             </div>
