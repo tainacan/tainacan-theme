d102ffe3aa3680b4244b62271b6c522f10992263
Fabiano <fabianocarvalho15@gmail.com>
2018-07-23 15:12:43 -0300

    Fix max large on title list blog and collections

--- src/template-parts/bannerHeader.php
+++ src/template-parts/bannerHeader.php
@@ -4,7 +4,7 @@
 ?>
 
 <div <?php if ( get_header_image() ) : ?>class="page-header header-filter clear-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter clear-filter align-items-center" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
-    <div class="container-fluid p-0 ph-title-description">
+    <div class="container-fluid max-large p-0 ph-title-description">
         <div class="bg-white-title title-header <?php if(is_singular() || is_archive()) { echo 'singular-title'; }?>">
             <h1 class="mb-0 text-truncate">
                 <?php if(is_home()) { ?> Blog <?php bloginfo('title'); }?>
