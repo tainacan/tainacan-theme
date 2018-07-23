2edf803766240b0cc7d8ae499f011477ea652843
Fabiano <fabianocarvalho15@gmail.com>
2018-07-23 16:05:00 -0300

    change function tainacan_post_date

--- src/functions/single-functions.php
+++ src/functions/single-functions.php
@@ -62,14 +62,10 @@ add_filter( 'comment_text', 'wrap_Comment', 99);
 	function tainacan_post_date() {
 		//if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-				$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> <time class="updated" datetime="%3$s">(updated %4$s)</time>';
-			}
+			
 			$time_string = sprintf( $time_string,
 				esc_attr( get_the_date( 'c' ) ),
-				get_the_date(),
-				esc_attr( get_the_modified_date( 'c' ) ),
-				get_the_modified_date()
+				get_the_date()
 			);
 			echo $time_string;
 		//}
