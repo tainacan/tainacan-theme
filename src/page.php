5c7acd9599c35abe8143559a74b7d9d2713fc2a3
Fabiano <fabianocarvalho15@gmail.com>
2018-07-27 16:53:23 -0300

    Revised Comment on Anonimous and Logged user

--- src/page.php
+++ src/page.php
@@ -8,7 +8,7 @@
 <div class="container-fluid mt-5 max-large">
     <div class="row">
 
-        <div class="col-sm">
+        <div class="col-sm margin-one-column">
             <div id="content" role="main">
                 <?php get_template_part('template-parts/loop', 'singular'); ?>
             </div><!-- /#content -->
