296d766a7623b5d040b4b8e342d3fc919739558e
Fabiano <fabianocarvalho15@gmail.com>
2018-08-03 15:03:55 -0300

    Changed form search in the navigation bar for better interaction.

--- src/header.php
+++ src/header.php
@@ -17,10 +17,23 @@
         <div class="container-fluid max-large px-0 margin-one-column" id="topNavbar">
             <?php echo tainacan_get_logo(); ?>
             <div class="btn-group ml-auto"> 
-                <?php 
-                    echo tainacan_get_form_search();
-                ?>
-                <button class="btn btn-link text-heavy-metal px-1 showInput"><i class="mdi mdi-magnify"></i></button>
+                    <form class="form-horizontal my-2 my-md-0 tainacan-search-form d-none d-md-block" [formGroup]="searchForm" role="form" (keyup.enter)="onSubmit()">
+                        <div class="input-group">
+                            <input type="text" name="s" placeholder="<?php _e('Search'); ?>" class="form-control" formControlName="searchText" size="50">
+                            <span class="text-heavy-metal input-group-btn mdi mdi-magnify form-control-feedback"></span>
+                        </div>
+                    </form>
+                    <div class="dropdown tainacan-form-dropdown d-md-none">
+                        <a class="btn btn-link text-heavy-metal px-1 dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="mdi mdi-magnify"></i></a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+                                <div class="input-group border">
+                                    <input class="form-control py-2 border-0" type="search" name="s" placeholder="<?php _e('Search'); ?>" id="tainacan-search">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 <!-- <button type="button" class="btn btn-link text-heavy-metal dropdown-toggle dropdown-toggle-split px-1 d-flex align-items-center" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="mdi mdi-account-outline"></i><div class="d-none d-md-inline-flex px-1">User</div><span class="sr-only text-jelly-bean">Toggle Dropdown</span>
                 </button>
