a9921db7e687787f6971d60c0f9a4f074adae125
Fabiano Alencar <fabianocarvalho15@gmail.com>
2018-08-13 14:33:50 -0300

    Realign the share button

--- src/template-parts/headerCollection.php
+++ src/template-parts/headerCollection.php
@@ -1,11 +1,11 @@
 <div <?php if ( get_header_image() ) : ?>class="page-header header-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else: ?>class="page-header header-filter page-collection" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/capa.png')"<?php endif; ?>>
     <div class="container-fluid px-0 t-bg-collection" style="<!-- z-index: 0; -->">
-        <div class="collection-header position-relative" style="">
+        <div class="collection-header position-relative max-large" style="">
             <?php if(has_post_thumbnail(tainacan_get_collection_id())) : ?>
-                <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid border border-white position-absolute text-left" style="bottom: -6rem; margin-left: 4.16666666667%;">
+                <img src="<?php echo get_the_post_thumbnail_url(tainacan_get_collection_id()); ?>" class="t-collection--info-img rounded-circle img-fluid border border-white position-absolute text-left" style="margin-left: 4.16666666667%;">
             <?php else : ?>
                 <div class="image-placeholder rounded-circle border border-white position-absolute">
-                    <h4 class="text-center"  style="bottom: -6rem; margin-left: 4.16666666667%;">
+                    <h4 class="text-center"  style="margin-left: 4.16666666667%;">
                     <?php 
                         echo tainacan_get_initials(tainacan_get_the_collection_name());
                     ?>
