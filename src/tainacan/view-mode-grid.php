cd9fe9c6d9e72871215a15b5ad100da940f3ac5d
mateuswetah <mateus.m.luna@gmail.com>
2018-09-27 14:44:11 -0300

    Replaces flex by grid layout on thumbnail view mode.

--- src/tainacan/view-mode-grid.php
+++ src/tainacan/view-mode-grid.php
@@ -2,23 +2,21 @@
 
 	<div class="tainacan-grid-container">
 
-		<div class="row no-gutters">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="col">
-				<a class="tainacan-grid-item" href="<?php the_permalink(); ?>">
-					<p class="metadata-title">     
-						<?php the_title(); ?>           
-					</p>
-					<?php if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail( 'tainacan-medium' ); ?> 
-					<?php else : ?>
-						<?php echo '<img alt="Thumbnail placeholder" src="' . get_template_directory_uri() . '/assets/images/thumbnail_placeholder.png">'?>
-					<?php endif; ?>  
-				</a>
-			</div>            
 			
+			<a class="tainacan-grid-item" href="<?php the_permalink(); ?>">
+				<p class="metadata-title">     
+					<?php the_title(); ?>           
+				</p>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail( 'tainacan-medium' ); ?> 
+				<?php else : ?>
+					<?php echo '<img alt="Thumbnail placeholder" src="' . get_template_directory_uri() . '/assets/images/thumbnail_placeholder.png">'?>
+				<?php endif; ?>  
+			</a>	
+		
 		<?php endwhile; ?>
-		</div>
+	
 	</div>
 
 <?php else : ?>
