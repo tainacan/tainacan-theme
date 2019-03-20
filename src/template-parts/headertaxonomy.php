dd89cdf2f5c88b5ccbcf17975133c2d410dd4a4e
Alvino Rodrigues <alvino.digo@gmail.com>
2019-03-20 18:33:43 -0300

    Alteracoes no cabecalho da pagina de taxonomia

--- src/template-parts/headertaxonomy.php
+++ src/template-parts/headertaxonomy.php
@@ -11,7 +11,7 @@ $image =  $current_term->get_header_image_id();
 $src = wp_get_attachment_image_src($image, 'full');
 ?>
 
-<div class="page-header-taxonomy max-large <?php if(!$src) : echo 'page-header-taxonomy--height-auto'; endif; ?>">
+<div class="page-header-taxonomy <?php if(!$src) : echo 'page-header-taxonomy--height-auto'; endif; ?>">
 
 	<div class="container-fluid m-0 p-0 d-flex flex-wrap">
 		<?php do_action( 'tainacan-interface-taxonomy-header' ); ?>
@@ -20,7 +20,7 @@ $src = wp_get_attachment_image_src($image, 'full');
 		<div class="col-12 col-sm-4 p-0 page-header-image" style="background-image: url('<?php echo $src[0]; ?>');"></div>
 		<?php endif; ?>
 		<div class="col-12 col-sm p-0 page-header-content">
-			<div class="page-header-content-meta">
+			<div class="page-header-content-meta max-large">
 				<div class="page-header-content-title d-inline-flex border-bottom">
 					<h2 class="page-header-title">
 						<?php echo $taxonomy->labels->name . ':'; ?>
