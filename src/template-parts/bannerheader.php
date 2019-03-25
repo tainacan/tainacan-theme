b98348ba14bffbc284389a05cf26f67ff9571f9d
mateuswetah <mateus.m.luna@gmail.com>
2019-03-25 15:31:32 -0300

    Adjusts website title alignment on large screens. Closes #14.

--- src/template-parts/bannerheader.php
+++ src/template-parts/bannerheader.php
@@ -1,5 +1,5 @@
 <div <?php if ( get_header_image() ) : ?>class="page-header header-filter clear-filter page-height" style="background-image: url('<?php header_image(); ?>')"<?php else : ?>class="page-header header-filter clear-filter align-items-center" style="background-image: url('<?php echo esc_url( get_template_directory_uri() ) ?>/assets/images/capa.png')"<?php endif; ?>>
-	<div class="container-fluid max-large p-0 ph-title-description">
+	<div class="container-fluid p-0 ph-title-description">
 		<div class="bg-white-title title-header <?php if ( is_singular() || is_archive() || is_search() || is_home() ) { echo 'singular-title'; }?>">
 			<h1 class="mb-0 text-truncate">
 				<?php
