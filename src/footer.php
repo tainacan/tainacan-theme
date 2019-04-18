cf16566a218df61b3838e908686c70330cfa7951
mateuswetah <mateus.m.luna@gmail.com>
2019-04-18 11:10:48 -0300

    Several fixes to footer css.

--- src/footer.php
+++ src/footer.php
@@ -1,16 +1,16 @@
 <?php if ( ! is_404() ) : ?>
 	<footer class="container-fluid p-4 p-sm-5 mt-5 tainacan-footer" style="padding-bottom: 0 !important;">
 		<?php if ( is_active_sidebar( 'tainacan-sidebar-footer' ) ) { ?>
-			<div class="row">
+			<div class="row tainacan-footer-widgets-area">
 				<div class="col-12 col-lg">
-					<ul class="p-4 d-lg-flex justify-content-md-center mb-md-0">
+					<ul class="pt-3 pb-3 pl-0 pr-0 d-lg-flex justify-content-xs-center mb-md-0">
 						<?php dynamic_sidebar( 'tainacan-sidebar-footer' ); ?>
 					</ul>
 				</div>
 			</div>
 		<?php } ?>
 		<hr class="bg-scooter"/>
-		<div class="row p-4 tainacan-footer-info">
+		<div class="row pt-3 pb-4 pl-0 pr-0 tainacan-footer-info">
 			<div class="col text-white font-weight-normal">
 				<p class="tainacan-footer-info--blog">
 					<?php echo bloginfo( 'title' );
@@ -22,21 +22,19 @@
 					if ( get_theme_mod( 'tainacan_blogaddress' ) ) {
 						echo wp_filter_nohtml_kses( get_theme_mod( 'tainacan_blogaddress', '' ) );
 					} ?>
-				</p>
-				<p class="tainacan-footer-info--blog">
-<?php if ( get_theme_mod( 'tainacan_blogemail' ) ) {
-	printf( __( 'E-mail: %s', 'tainacan-interface' ), sanitize_email( get_theme_mod( 'tainacan_blogemail', '' ) ) );
-}
-if ( get_theme_mod( 'tainacan_blogemail' ) && get_theme_mod( 'tainacan_blogphone' ) ) {
-	if ( wp_is_mobile() ) :
-		echo '<br>';
-	else :
-		echo ' - ';
-	endif;
-}
-if ( get_theme_mod( 'tainacan_blogphone' ) ) {
-	printf( __( 'Telephone: %s', 'tainacan-interface' ), wp_filter_nohtml_kses( get_theme_mod( 'tainacan_blogphone', '' ) ) );
-} ?>
+					<?php if ( get_theme_mod( 'tainacan_blogemail' ) ) {
+						printf( __( 'E-mail: %s', 'tainacan-interface' ), sanitize_email( get_theme_mod( 'tainacan_blogemail', '' ) ) );
+					}
+					if ( get_theme_mod( 'tainacan_blogemail' ) && get_theme_mod( 'tainacan_blogphone' ) ) {
+						if ( wp_is_mobile() ) :
+							echo '<br>';
+						else :
+							echo ' - ';
+						endif;
+					}
+					if ( get_theme_mod( 'tainacan_blogphone' ) ) {
+						printf( __( 'Telephone: %s', 'tainacan-interface' ), wp_filter_nohtml_kses( get_theme_mod( 'tainacan_blogphone', '' ) ) );
+					} ?>
 				</p>
 			</div>
 			<div class="col-auto pr-0 pr-md-3 d-none d-md-block align-self-md-top">
