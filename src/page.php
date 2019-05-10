a62a45977229c2061c25ff6d016cc7e67193c5c0
mateuswetah <mateus.m.luna@gmail.com>
2019-05-10 13:03:57 -0300

    Same container classes for pages and posts. This fixes an error where some wide gutenberg blocks would overlap left margin.

--- src/page.php
+++ src/page.php
@@ -3,10 +3,9 @@
 <!-- Get the banner to display -->
 <?php get_template_part( 'template-parts/bannerheader' ); ?>
 
-<div class="container-fluid mt-5 max-large">
+<div class="mt-5 max-large margin-one-column">
 	<div class="row">
-
-		<div class="col-sm margin-one-column">
+		<div class="col col-sm mx-sm-auto">
 			<div id="content" role="main">
 				<?php get_template_part( 'template-parts/loop', 'singular' ); ?>
 			</div><!-- /#content -->
