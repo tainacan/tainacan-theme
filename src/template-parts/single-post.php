cd9d38833463b1f75e4874a43ce066e974eb2efa
mateuswetah <mateus.m.luna@gmail.com>
2019-10-07 10:54:56 -0300

    Removes title, meta and top margin when no title is passed to posts or pages. #20.

--- src/template-parts/single-post.php
+++ src/template-parts/single-post.php
@@ -1,26 +1,28 @@
 <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
-	<header class="mb-4">
-		<div class="header-meta text-muted mb-5 d-flex">
-			<?php if ( ! is_singular( 'page' ) ) { ?>
-				<?php tainacan_meta_date_author(); ?>
-			<?php } ?>
-			<div class="btn-group ml-auto" role="group">
-				<?php if ( true == get_theme_mod( 'tainacan_facebook_share', true ) ) : ?> 
-					<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" class="" target="_blank">
-						<img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/facebook-circle.png'; ?>" alt="<?php esc_attr_e('Share this on facebook', 'tainacan-interface') ?>">
-					</a>
-				<?php endif; ?>
-				<?php if ( true == get_theme_mod( 'tainacan_twitter_share', true ) ) : ?> 
-					<?php $twitter_option = get_theme_mod( 'tainacan_twitter_user' ); ?>
-					<?php $via = ! empty( $twitter_option ) ? '&amp;via=' . esc_attr( get_theme_mod( 'tainacan_twitter_user' ) ) : ''; ?>
-					<a href="http://twitter.com/share?url=<?php the_permalink(); ?>&amp;text=<?php the_title_attribute(); ?><?php echo $via; ?>" target="_blank" class="">
-						<img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/twitter-circle.png'; ?>" alt="<?php esc_attr_e('Share this on twitter', 'tainacan-interface') ?>">
-					</a>
-				<?php endif; ?>
+	<?php if ( the_title()  != '') : ?>
+		<header class="mb-4">
+			<div class="header-meta text-muted mb-5 d-flex">
+				<?php if ( ! is_singular( 'page' ) ) { ?>
+					<?php tainacan_meta_date_author(); ?>
+				<?php } ?>
+				<div class="btn-group ml-auto" role="group">
+					<?php if ( true == get_theme_mod( 'tainacan_facebook_share', true ) ) : ?> 
+						<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" class="" target="_blank">
+							<img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/facebook-circle.png'; ?>" alt="<?php esc_attr_e('Share this on facebook', 'tainacan-interface') ?>">
+						</a>
+					<?php endif; ?>
+					<?php if ( true == get_theme_mod( 'tainacan_twitter_share', true ) ) : ?> 
+						<?php $twitter_option = get_theme_mod( 'tainacan_twitter_user' ); ?>
+						<?php $via = ! empty( $twitter_option ) ? '&amp;via=' . esc_attr( get_theme_mod( 'tainacan_twitter_user' ) ) : ''; ?>
+						<a href="http://twitter.com/share?url=<?php the_permalink(); ?>&amp;text=<?php the_title_attribute(); ?><?php echo $via; ?>" target="_blank" class="">
+							<img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/twitter-circle.png'; ?>" alt="<?php esc_attr_e('Share this on twitter', 'tainacan-interface') ?>">
+						</a>
+					<?php endif; ?>
+				</div>
 			</div>
-		</div>
-		<?php the_post_thumbnail(); ?>
-	</header>
+			<?php the_post_thumbnail(); ?>
+		</header>
+	<?php endif; ?>
 	<section class="tainacan-content text-black margin-two-column">
 		<?php
 			the_content();
