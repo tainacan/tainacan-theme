b49037c3284c9f3edf2815285b1dd0151b377ac6
mateuswetah <mateus.m.luna@gmail.com>
2019-11-18 09:32:14 -0300

    Wraps all Tainacan Titles in H1 elements. Adjusts margint bottom oh title and margin left of lists.

--- src/template-parts/loop.php
+++ src/template-parts/loop.php
@@ -1,20 +1,22 @@
 <?php if ( have_posts() ) : ?>
 	<div class="tainacan-title">
-		<div class="border-bottom border-jelly-bean tainacan-title-page">
+		<div class="border-bottom border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
 			<ul class="list-inline mb-1 d-flex">
 				<li class="list-inline-item text-midnight-blue font-weight-bold title-page">
+					<h1>
 					<?php if ( is_home() ) {
 						if ( get_option( 'page_for_posts' ) ) :
 							echo get_the_title( get_option( 'page_for_posts' ) );
 						else :
 							_e( 'Blog Posts', 'tainacan-interface' );
 						endif;
-} elseif ( is_search() ) {
-	_e( 'Search Results for', 'tainacan-interface' );
-	echo ' "' , get_search_query(), '"';
-} elseif ( is_archive() ) {
-	echo ' ' . get_the_archive_title();
-} ?>
+					} elseif ( is_search() ) {
+						_e( 'Search Results for', 'tainacan-interface' );
+						echo ' "' , get_search_query(), '"';
+					} elseif ( is_archive() ) {
+						echo ' ' . get_the_archive_title();
+					} ?>
+					</h1>
 				</li>
 				<li class="list-inline-item float-right title-back align-self-end ml-auto"><a href="javascript:history.go(-1)"><?php _e( 'Back', 'tainacan-interface' ); ?></a></li>
 			</ul>
