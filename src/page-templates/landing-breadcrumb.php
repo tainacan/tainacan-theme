10511d02adbd04d5e3f5d5147669f6cd6fa57804
mateuswetah <mateus.m.luna@gmail.com>
2020-07-14 16:57:34 -0300

    Removes all padding from landing template pages

--- src/page-templates/landing-breadcrumb.php
+++ src/page-templates/landing-breadcrumb.php
@@ -3,12 +3,8 @@
 <?php get_header(); ?>
 
 <div class="max-large margin-one-column">
-	<div class="row">
-		<div class="col col-sm mx-sm-auto">
-			<div id="content" role="main">
-				<?php get_template_part( 'template-parts/loop', 'singular' ); ?>
-			</div><!-- /#content -->
-		</div>
-	</div><!-- /.row -->
+	<div id="content" role="main">
+		<?php get_template_part( 'template-parts/loop', 'singular' ); ?>
+	</div><!-- /#content -->
 </div>
 <?php get_footer(); ?>
