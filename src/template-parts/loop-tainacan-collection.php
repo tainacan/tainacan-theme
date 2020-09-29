b2cd43868c8382d24bf1212927649aa3806de726
mateuswetah <mateus.m.luna@gmail.com>
2020-09-29 18:50:52 -0300

    Updates 'description not informed' label. Closes #35

--- src/template-parts/loop-tainacan-collection.php
+++ src/template-parts/loop-tainacan-collection.php
@@ -21,7 +21,7 @@
 								<?php if ( get_the_excerpt() ) : ?>
 									<p><?php echo wp_trim_words( get_the_excerpt(), 35, '[...]' ); ?></p>
 								<?php else : ?>
-									<p style="font-style: italic;"><?php _e( 'Description not informed', 'tainacan-interface' ); ?></p>
+									<p style="font-style: italic;"><?php _e( 'No description provided', 'tainacan-interface' ); ?></p>
 								<?php endif; ?>
 							</div>
 						</div>
