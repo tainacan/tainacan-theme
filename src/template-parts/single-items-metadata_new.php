7f88f70963d808a275fb370e2a1bac189fb49fed
mateuswetah <mateus.m.luna@gmail.com>
2025-03-06 09:32:49 -0300

    Fixes bug in accordeon layout mode for metadata sections.

--- src/template-parts/single-items-metadata_new.php
+++ src/template-parts/single-items-metadata_new.php
@@ -190,7 +190,6 @@
         $args = array(
             'before' => '',
             'after' => '',
-            'metadata_sections__not_in' => ['default_section'],
             'before_name' => '<input name="accordion" type="radio" id="accordion-section-$id"/>
                         <label for="accordion-section-$id">
                             <i class="tainacan-icon tainacan-icon-arrowright tainacan-icon-30px"></i>
